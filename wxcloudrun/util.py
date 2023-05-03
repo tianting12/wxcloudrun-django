d6fa79f5eccac226facebdcfee2f8e503fe7ef55
Dath <1162467606@qq.com>
2023-05-03 21:16:51 +0800

    小有成效

--- wxcloudrun/util.py
+++ wxcloudrun/util.py
@@ -1,10 +1,15 @@
 import os
+import time
+from urllib.parse import urlparse
 
 import openai
 import requests
+import validators
+import xmltodict
 
 headers = {
-    "Cookie": "buvid3=624DC525-E7B5-0416-1735-7CD7AF68A3A245957infoc; i-wanna-go-back=-1; _uuid=D812FC103-1B8D-288E-A16A-D395A6581082645694infoc; buvid4=505CDFDF-0FEE-9CD2-F219-129A152C10CA46623-022071123-cWZGbE4cldom6m5ltwQ11A==; buvid_fp_plain=undefined; LIVE_BUVID=AUTO5716575551770605; CURRENT_BLACKGAP=0; DedeUserID=361453208; DedeUserID__ckMd5=4cb1e81e3095816d; nostalgia_conf=-1; blackside_state=0; b_ut=5; is-2022-channel=1; fingerprint3=adff839f4ad592c1c87e6205cb0ef759; hit-dyn-v2=1; b_nut=100; rpdid=|(k|k)~u|mYR0J'uYY)mullYu; CURRENT_QUALITY=120; header_theme_version=CLOSE; home_feed_column=5; CURRENT_FNVAL=4048; CURRENT_PID=d6815990-cf09-11ed-9502-f922cc9137c4; FEED_LIVE_VERSION=V8; browser_resolution=2560-1289; hit-new-style-dyn=0; bsource=search_baidu; fingerprint=606cee3cf376d013849ff9b45826538d; buvid_fp=606cee3cf376d013849ff9b45826538d; PVID=1; SESSDATA=0c4e71ce,1698493717,6baa2*51; bili_jct=b139ffb592d46dbe26ce7dd47ea61ceb; sid=8njneye0; innersign=1; b_lsid=AD31C7DC_187D866137F; bp_video_offset_361453208=790810144955957200"}
+    "Cookie": "buvid3=624DC525-E7B5-0416-1735-7CD7AF68A3A245957infoc; i-wanna-go-back=-1; _uuid=D812FC103-1B8D-288E-A16A-D395A6581082645694infoc; buvid4=505CDFDF-0FEE-9CD2-F219-129A152C10CA46623-022071123-cWZGbE4cldom6m5ltwQ11A==; buvid_fp_plain=undefined; LIVE_BUVID=AUTO5716575551770605; CURRENT_BLACKGAP=0; DedeUserID=361453208; DedeUserID__ckMd5=4cb1e81e3095816d; nostalgia_conf=-1; blackside_state=0; b_ut=5; is-2022-channel=1; fingerprint3=adff839f4ad592c1c87e6205cb0ef759; hit-dyn-v2=1; b_nut=100; rpdid=|(k|k)~u|mYR0J'uYY)mullYu; CURRENT_QUALITY=120; header_theme_version=CLOSE; home_feed_column=5; CURRENT_FNVAL=4048; CURRENT_PID=d6815990-cf09-11ed-9502-f922cc9137c4; FEED_LIVE_VERSION=V8; browser_resolution=2560-1289; hit-new-style-dyn=0; bsource=search_baidu; fingerprint=606cee3cf376d013849ff9b45826538d; buvid_fp=606cee3cf376d013849ff9b45826538d; PVID=1; bp_video_offset_361453208=790810144955957200; b_lsid=31E107EA1_187E1669532; innersign=1; SESSDATA=e5a3fb70,1698666620,a4a5f*51; bili_jct=82382050fc93baec7f533ee817d829dc; sid=7mkgefpx"
+}
 
 
 def bili_info(bvid):
@@ -79,8 +84,9 @@ def chat(text, ):
     prompt = '作为一名专业的视频内容编辑，你的任务是总结以下视频字幕文本的精华内容，并以无序列表的方式返回。请确保所有句子都足够简洁、清晰和完整。 ' \
              '注意，你需要理解并概括原始文本中的主要信息和关键点，并将其转化成易于理解和记忆的形式。' \
              '同时，请确保你所提供的摘要准确反映了原始文本中所述内容。'
-    # openai.api_key = os.environ.get("API_KEY")
-    openai.api_key = "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"
+    openai.api_key = os.environ.get("API_KEY")
+    openai.api_key = os.environ.get("API_BASE")
+
     completions = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
@@ -91,3 +97,38 @@ def chat(text, ):
 
     ans = completions.choices[0].message.content
     return ans
+
+
+def pack_msg(reply_info: dict, msg: str) -> str:
+    resp_dict = {
+        "xml": {
+            'ToUserName': reply_info.get('FromUserName'),
+            'FromUserName': reply_info.get('ToUserName'),
+            'CreateTime': int(time.time()),
+            'MsgType': 'text',
+            'Content': msg
+        }
+    }
+    return xmltodict.unparse(resp_dict)
+
+
+def get_bvId(url):
+    parsed_url = urlparse(url)
+    print(parsed_url)
+    bvid = parsed_url.path.split('/')[-1]
+    if not bvid:
+        bvid = parsed_url.path.split('/')[-2]
+    return bvid
+
+
+def is_bilibili_link(link):
+    # 判断链接是否符合 URL 格式
+    if not validators.url(link):
+        return False
+
+    # 解析链接，获取其域名
+    domain = urlparse(link).netloc
+
+    # 判断域名是否为 bilibili.com
+    if domain == 'www.bilibili.com' or domain == 'm.bilibili.com':
+        return True
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,11 +1,12 @@
 import json
 import logging
+import time
 
 from django.http import JsonResponse
 from django.shortcuts import render
 import xml.etree.ElementTree as ET
 from django.http import HttpResponse
-from wxcloudrun.util import chat
+from wxcloudrun.util import chat, pack_msg, is_bilibili_link, get_bvId, bili_player_list, bili_subtitle, segTranscipt
 
 logger = logging.getLogger('log')
 
@@ -24,12 +25,14 @@ def demo(request):
     da = json.loads(request.body)
     print(da)
     data = []
+
     return JsonResponse({'code': 0, 'data': data},
                         json_dumps_params={'ensure_ascii': False})
 
 
 def my_view(request):
     # 构造XML元素树
+    da = json.loads(request.body)
     xml_tree = ET.Element('xml')
     ET.SubElement(xml_tree, 'ToUserName').text = 'toUser'
     ET.SubElement(xml_tree, 'FromUserName').text = 'fromUser'
@@ -48,8 +51,41 @@ def my_view(request):
 
 
 def chat1(request):
-    data = []
+    reply_info = json.loads(request.body)
+    blink = reply_info["Content"]
+    check = is_bilibili_link(blink)
+    if not check:
+        print("pass,url 错误")
+    bvid = get_bvId(blink)
 
-    content = chat("")
-    return JsonResponse({'code': 0, 'data': data, "message": content},
-                        json_dumps_params={'ensure_ascii': False})
+    cid, title = bili_player_list(bvid)
+    transcript_text = bili_subtitle(bvid, cid)
+    summarized_text = ''
+    if transcript_text:
+        print('字幕获取成功')
+        seged_text = segTranscipt(transcript_text)
+        i = 1
+        print(seged_text)
+        for entry in seged_text:
+            try:
+                response = chat(entry)
+                print(response)
+                print(f'完成第{str(i)}部分摘要')
+                print()
+                i += 1
+            except  Exception as e:
+                print('GPT接口摘要失败, 请检查网络连接', e)
+                response = '摘要失败'
+            summarized_text += '\n' + response
+        # insert2notion(token, database_id, bvid, summarized_text)
+    else:
+        print('字幕获取失败')
+
+    if not summarized_text:
+        summarized_text = '总结失败'
+
+    print(summarized_text)
+
+    xml_str = pack_msg(reply_info, summarized_text)
+
+    return HttpResponse(xml_str, content_type='application/xml')
